cdb93c454220d4b4dc31f7bdec52984fc7f5ec80
oscar-otee <oluoch.oscar.oo@gmail.com>
2022-06-05 19:22:29 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+// function for moving dodger left:
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+// function for moving dodger right:
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+
+// Event listener function:
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowLeft") {
+        moveDodgerRight();
+    }
+});
